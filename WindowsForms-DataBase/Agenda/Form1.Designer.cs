170f592b769df082e294390e053a23b9cf01c676
FranciscoSabato <francisco_francisco123@outlook.com>
2019-08-29 22:16:02 -0300

    projeto de ligação com banco acces, sistema de agenda

---
+++ WindowsForms-DataBase/Agenda/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Agenda
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtNovoNome = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnAlterar = new System.Windows.Forms.Button();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.txtInformacoes = new System.Windows.Forms.TextBox();
+            this.txtNovoTelefone = new System.Windows.Forms.TextBox();
+            this.txtTelefone = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nome:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Novo Nome:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(16, 27);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(204, 20);
+            this.txtNome.TabIndex = 2;
+            // 
+            // txtNovoNome
+            // 
+            this.txtNovoNome.Location = new System.Drawing.Point(16, 109);
+            this.txtNovoNome.Name = "txtNovoNome";
+            this.txtNovoNome.Size = new System.Drawing.Size(204, 20);
+            this.txtNovoNome.TabIndex = 3;
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(226, 24);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(75, 23);
+            this.btnCadastrar.TabIndex = 4;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(226, 65);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(75, 23);
+            this.btnExcluir.TabIndex = 5;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnAlterar
+            // 
+            this.btnAlterar.Location = new System.Drawing.Point(226, 109);
+            this.btnAlterar.Name = "btnAlterar";
+            this.btnAlterar.Size = new System.Drawing.Size(75, 23);
+            this.btnAlterar.TabIndex = 6;
+            this.btnAlterar.Text = "Alterar";
+            this.btnAlterar.UseVisualStyleBackColor = true;
+            this.btnAlterar.Click += new System.EventHandler(this.btnAlterar_Click);
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(226, 150);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(75, 23);
+            this.btnConsultar.TabIndex = 7;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // txtInformacoes
+            // 
+            this.txtInformacoes.Location = new System.Drawing.Point(16, 188);
+            this.txtInformacoes.Multiline = true;
+            this.txtInformacoes.Name = "txtInformacoes";
+            this.txtInformacoes.Size = new System.Drawing.Size(285, 224);
+            this.txtInformacoes.TabIndex = 8;
+            // 
+            // txtNovoTelefone
+            // 
+            this.txtNovoTelefone.Location = new System.Drawing.Point(16, 153);
+            this.txtNovoTelefone.Name = "txtNovoTelefone";
+            this.txtNovoTelefone.Size = new System.Drawing.Size(204, 20);
+            this.txtNovoTelefone.TabIndex = 12;
+            // 
+            // txtTelefone
+            // 
+            this.txtTelefone.Location = new System.Drawing.Point(16, 68);
+            this.txtTelefone.Name = "txtTelefone";
+            this.txtTelefone.Size = new System.Drawing.Size(204, 20);
+            this.txtTelefone.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 137);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Novo Telefone:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(13, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Telefone:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(315, 424);
+            this.Controls.Add(this.txtNovoTelefone);
+            this.Controls.Add(this.txtTelefone);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtInformacoes);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.btnAlterar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtNovoNome);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtNovoNome;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnAlterar;
+        private System.Windows.Forms.Button btnConsultar;
+        private System.Windows.Forms.TextBox txtInformacoes;
+        private System.Windows.Forms.TextBox txtNovoTelefone;
+        private System.Windows.Forms.TextBox txtTelefone;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsForms-DataBase/Agenda/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Agenda
+{
+    public partial class Form1 : Form
+    {
+        OleDbConnection conn = new OleDbConnection();
+        String connect = @"Provider=Microsoft.ACE.OLEDB.12.0;
+                Data Source=C:\Users\francisco.hsfsouza\Documents\teste.accdb";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            if(txtNome.Text != String.Empty && txtTelefone.Text != String.Empty)
+            { 
+            operacao_banco("INSERT INTO tabela1(nome,telefone) VALUES('" + txtNome.Text + "' , '" + txtTelefone.Text + "')");
+            }
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            operacao_banco("DELETE FROM tabela1 WHERE nome = ('" + txtNome + "')");
+        }
+
+        private void btnAlterar_Click(object sender, EventArgs e)
+        {
+            operacao_banco("UPDATE tabela1 SET nome = ('" + txtNovoNome.Text + "') WHERE nome = ('" + txtNome.Text + "')");
+            operacao_banco("UPDATE tabela1 SET telefone = ('" + txtNovoTelefone.Text + "') WHERE telefone = ('" + txtTelefone.Text + "')");
+        }
+
+
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            try
+            {                                 
+                conn.ConnectionString = connect;
+                OleDbCommand cmd = new OleDbCommand("SELECT * FROM tabela1 WHERE nome Like('"+txtNome.Text+ "%')ORDER BY nome ASC");
+                
+                cmd.Connection = conn;
+
+                conn.Open();
+                OleDbDataReader aReader = cmd.ExecuteReader();
+                txtInformacoes.Text = "Os Valores Retornados da Tabela São: ";
+                while (aReader.Read())
+                {
+                    txtInformacoes.Text += Environment.NewLine + aReader.GetString(0) + " " + aReader.GetInt32(1);
+                }
+                conn.Close();
+            }
+            catch (Exception Erro)
+            {
+                MessageBox.Show("Dado Não Apagado, Erro: " + Erro.ToString(), "Atenção", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+        }
+
+
+
+        void operacao_banco(String op)
+        {            
+                try
+                {
+                    conn.ConnectionString = connect;
+                    OleDbCommand cmd = new OleDbCommand(op);
+                    cmd.Connection = conn;
+
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                    MessageBox.Show("Dado Cadastrado Com Sucesso!", "Sucesso",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    conn.Close();
+                }
+                catch (Exception Erro)
+                {
+                    MessageBox.Show("Dado Não Cadastrado, Erro: " + Erro.ToString(), "Atenção", MessageBoxButtons.OK,
+                        MessageBoxIcon.Error);
+                }                                                                                                                       
+        }
+    }
+}
